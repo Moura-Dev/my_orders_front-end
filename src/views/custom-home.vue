07572d154fc49d971f84c0282077d48716bf87ea
Lucas Senna <tarcisio.almeida197@gmail.com>
2022-04-26 15:13:13 -0300

    update barchart

--- src/views/custom-home.vue
+++ src/views/custom-home.vue
@@ -1,94 +1,32 @@
 <template>
-  <div>
-    <h1 class="text-center">Estatísticas</h1>
-    <div class="stats">
-      <Bar
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        :chart-id="chartId"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width="width"
-        :height="height"
-      />
+  <div id="home">
+    <h1 class="text-center pt-2">Estatísticas</h1>
+    <div class="chartbar">
+      <BarChart />
     </div>
   </div>
 </template>
 
 <script>
-import { Bar } from "vue-chartjs/legacy";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
-
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-);
+import BarChart from "../components/custom-bar.vue";
 
 export default {
+  name: "App",
   components: {
-    Bar,
-  },
-  props: {
-    chartId: {
-      type: String,
-      default: "bar-chart",
-    },
-    datasetIdKey: {
-      type: String,
-      default: "label",
-    },
-    width: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 230,
-    },
-    cssClasses: {
-      default: "",
-      type: String,
-    },
-    styles: {
-      type: Object,
-      default: () => {},
-    },
-    plugins: {
-      type: Object,
-      default: () => {},
-    },
-  },
-  data() {
-    return {
-      chartData: {
-        labels: ["Contratos", "Vendas", "Expirados"],
-        datasets: [{ data: [40, 20, 12] }],
-      },
-      chartOptions: {
-        responsive: true,
-      },
-    };
+    BarChart,
   },
 };
 </script>
 
 <style scoped>
-.stats {
-  max-width: 70%;
+#home {
+  width: 100%;
+  height: 100%;
+}
+
+.chartbar {
+  max-width: 90%;
   margin: 0 auto;
+  padding-top: 3em;
 }
 </style>
